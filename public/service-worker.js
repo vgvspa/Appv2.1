2ef136114ad30c9f851a717c45de9f81cd3bb27d
vgvspa <MAURO.OLEA@VGV.CL>
2026-05-28 11:37:30 -0400

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "vgv-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script_v2.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
